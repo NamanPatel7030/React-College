700f352688b32af072a1b1feb1ca136a69a91374
Naman Developer <namandeveloper7030@gmail.com>
2024-12-24 10:28:34 +0530

    working first project time table

---
+++ first react app/src/Lecture24-12/comp2.jsx
@@ -0,0 +1,18 @@
+import * as react from 'react';
+
+class inputField extends React.Component {
+
+render() {
+    return(
+    <div>
+    <span>
+        Text:- <input type="text"></input>
+    </span>
+    </div>
+)
+
+}
+
+}
+
+export default inputField;
