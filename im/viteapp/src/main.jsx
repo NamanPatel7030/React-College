c6fac8cb40433aaee76c5f9a6c6cf57f363c062b
Naman Developer <namandeveloper7030@gmail.com>
2024-12-26 10:08:35 +0530

    changes

--- im/viteapp/src/main.jsx
+++ im/viteapp/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './files/f241224.jsx'
+import App from './files/Timetable.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
